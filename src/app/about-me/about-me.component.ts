e2fc69eeff430cb07223c27b4b924695abef1db3
Chalermchai Wongfiang <chalermchai@cardx.co.th>
2025-01-03 14:05:14 +0700

    feat: update profile 2024

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -4,54 +4,96 @@ import Typed, { TypedOptions } from 'typed.js';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
 })
 export class AboutMeComponent implements OnInit {
-
   Shell = require('shell.js');
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.initCommand();  
+    this.initCommand();
   }
+
+  calculateAge = () => {
+    var dob = new Date(1995, 10, 13);
+    var currentDate = new Date();
+
+    let age = currentDate.getFullYear() - dob.getFullYear();
+    const monthDifference = currentDate.getMonth() - dob.getMonth();
+
+    // Adjust age if the birthday hasn't occurred yet this year
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && currentDate.getDate() < dob.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  };
+
+  headerCommand = () => {
+    return [
+      "<b>&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #ffa929'>__&nbsp;&nbsp;__</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </b>",
+      "<b>&nbsp;&nbsp;/_\\&nbsp;|&nbsp;|__&nbsp;&nbsp;___&nbsp;_&nbsp;&nbsp;_|&nbsp;|_&nbsp;&nbsp;<span style='color: #ffa929'>|&nbsp;&nbsp;\\/&nbsp;&nbsp;|___&nbsp;</span></b>",
+      "<b>&nbsp;/&nbsp;_&nbsp;\\|&nbsp;'_&nbsp;\\/&nbsp;_&nbsp;\\&nbsp;||&nbsp;|&nbsp;&nbsp;_|&nbsp;<span style='color: #ffa929'>|&nbsp;|\\/|&nbsp;/&nbsp;-_) </span></b>",
+      "<b>/_/&nbsp;\\_\\_.__/\\___/\\_,_|\\__|&nbsp;<span style='color: #ffa929'>|_|&nbsp;&nbsp;|_\\___| </span></b>",
+      '<b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </b>',
+    ];
+  };
+
+  linebrakeCommand = () => {
+    return ["<span style='color: #51c2d5;'>#####################################</span>"]
+  };
+
+  profileCommand = () => {
+    var command: string[] = [];
+    Object.entries(this.getProfileDetail()).forEach(([key, value]) => {
+      command.push(`<span style='color: #8ad733'>${key}</span>: ${value}`);
+    });
+
+    return command;
+  };
+
+  getProfileDetail = () => {
+    return {
+      name: 'Chalerrmchai Wongfiang',
+      nickname: 'neung',
+      dateOfBirth: '13 Nov 1995',
+      age: this.calculateAge() + ' years old',
+      gender: 'male',
+      email: 'chalermchai.wp@gmail.com',
+      tel: '0624595263',
+      position: 'Fullstack Developer',
+    };
+  };
 
   initCommand() {
+    let command: (string[])[] = [];
+    command.push(this.headerCommand());
+    command.push(this.linebrakeCommand());
+    command.push(this.profileCommand());
+    command.push(this.linebrakeCommand());
+    command.push(['....']);
+
+    console.log(command)
 
     var option = {
-        user: 'root',
-        host: 'chalermchai.dev',
-        path: '/~/',
-        theme: 'dark',
-        style: 'osx',
-        responsive: true,
-        commands: [
-          "<b>&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #ffa929'>__&nbsp;&nbsp;__</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </b>",
-          "<b>&nbsp;&nbsp;/_\\&nbsp;|&nbsp;|__&nbsp;&nbsp;___&nbsp;_&nbsp;&nbsp;_|&nbsp;|_&nbsp;&nbsp;<span style='color: #ffa929'>|&nbsp;&nbsp;\\/&nbsp;&nbsp;|___&nbsp;</span></b>",
-          "<b>&nbsp;/&nbsp;_&nbsp;\\|&nbsp;'_&nbsp;\\/&nbsp;_&nbsp;\\&nbsp;||&nbsp;|&nbsp;&nbsp;_|&nbsp;<span style='color: #ffa929'>|&nbsp;|\\/|&nbsp;/&nbsp;-_) </span></b>",
-          "<b>/_/&nbsp;\\_\\_.__/\\___/\\_,_|\\__|&nbsp;<span style='color: #ffa929'>|_|&nbsp;&nbsp;|_\\___| </span></b>",
-          "<b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </b>",
-          "<span style='color: #51c2d5;'>#####################################</span>",
-          "<span style='color: #8ad733'>name</span>: Chalerrmchai Wongfiang",
-          "<span style='color: #8ad733'>nickname</span>: neung",
-          "<span style='color: #8ad733'>dateOfBirth</span>: 13 Nov 1995",
-          "<span style='color: #8ad733'>age</span>: 26 years old",
-          "<span style='color: #8ad733'>gender</span>: male",
-          "<span style='color: #8ad733'>email</span>: chalermchai.wp@gmail.com",
-          "<span style='color: #8ad733'>tel</span>: 062-459-5263",
-          "<span style='color: #8ad733'>position</span>: Full Stack Developer",
-          "<span style='color: #51c2d5;'>#####################################</span>",
-          '...',
-        ],
-        root: false,
-        typed: Typed,
-        typedSpeed: 5,
-        typedDelay: 50,
-        typedLoop:  false
-      }
+      user: 'root',
+      host: 'chalermchai.dev',
+      path: '/~/',
+      theme: 'dark',
+      style: 'osx',
+      responsive: true,
+      commands: command.flatMap(innerArray => innerArray),
+      root: false,
+      typed: Typed,
+      typedSpeed: 5,
+      typedDelay: 50,
+      typedLoop: false,
+    };
 
     new this.Shell('#awesome-shell', option);
-
   }
-
 }
--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -9,8 +9,53 @@ import { PrimeIcons } from 'primeng/api';
 export class WorkExperienceComponent implements OnInit {
   experience: any[] = [];
   // aos = require('aos');
- 
+
   constructor() {}
+
+  calculateAge = (start: Date, end: Date) => {
+    var monthUnit = 'mo';
+    var yearUnit = 'yr';
+
+    // Calculate the year and month difference
+    let years = end.getFullYear() - start.getFullYear();
+    let months = end.getMonth() - start.getMonth();
+
+    // Adjust if the difference in months is negative
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    // Include the current month if the end date is greater than or equal to the start date's day
+    if (end.getDate() >= start.getDate()) {
+      months++;
+    }
+
+    // Adjust if months exceeds 12
+    if (months >= 12) {
+      years++;
+      months -= 12;
+    }
+
+    if (months > 1) {
+      monthUnit += 's';
+    }
+    if (years > 1) {
+      yearUnit += 's';
+    }
+
+    if (years === 0) {
+      return `${months} ${monthUnit}`;
+    }
+
+    if (months === 0) {
+      return `${years} ${yearUnit}`;
+    }
+
+    return `${years} ${yearUnit} , ${months} ${monthUnit}`;
+
+    // return { years, months };
+  };
 
   ngOnInit() {
     // this.aos.init();
@@ -21,9 +66,10 @@ export class WorkExperienceComponent implements OnInit {
         name: 'Burapha University',
         logo: 'ng-logo-buu',
         year: '2015 - 2016',
+        age: this.calculateAge(new Date(2015, 0, 1), new Date(2016, 0, 1)),
         url: 'https://www.buu.ac.th/',
         icon: PrimeIcons.BOOK,
-        color: "#FF9800",
+        color: '#FF9800',
         systems: [
           {
             name: 'Inventory Management System ',
@@ -42,10 +88,11 @@ export class WorkExperienceComponent implements OnInit {
       {
         name: 'CDG Systems Ltd.',
         logo: 'ng-logo-cdg',
-        year: '2017 - 2021',
+        year: 'Jul 2018 - Jun 2021',
+        age: this.calculateAge(new Date(2018, 6, 1), new Date(2021, 5, 30)),
         url: 'https://www.cdg.co.th/',
         icon: PrimeIcons.BUILDING,
-        color: "#337",
+        color: '#004e98',
         systems: [
           {
             name: 'Programmer Allocation Management System',
@@ -65,14 +112,14 @@ export class WorkExperienceComponent implements OnInit {
             year: '2019',
             position: 'FullStack Developer ',
             tools: 'Java EE Angular PrimeNG',
-            url: 'https://dld.go.th'
+            url: 'https://dld.go.th',
           },
           {
             name: 'DLPW - Department of Labour Protection and Welfare',
             year: '2020',
             position: 'FullStack Developer ',
             tools: 'Java Struts Java EE GraphQL Angular PrimeNG',
-            url: 'https://www.labour.go.th/'
+            url: 'https://www.labour.go.th/',
           },
           {
             name: 'Timesheet - inhouse production',
@@ -85,27 +132,94 @@ export class WorkExperienceComponent implements OnInit {
             name: 'C-Drive MyCar',
             year: '2021',
             position: 'FullStack Developer ',
-            tools: 'Quarkus Java EE GraphQL Angular PrimeNG RabbitMQ ElasticSearch Redisson CI/CD deployment Jenkins Kubernetes Docker NodeJS JasperReports',
-            url: 'https://play.google.com/store/apps/details?id=th.co.cdgs.cdrive.mycar'
+            tools:
+              'Quarkus Java EE GraphQL Angular PrimeNG RabbitMQ ElasticSearch Redisson CI/CD deployment Jenkins Kubernetes Docker NodeJS JasperReports',
+            url: 'https://play.google.com/store/apps/details?id=th.co.cdgs.cdrive.mycar',
           },
         ],
       },
       {
-        name: 'Cube SoftTech Co., Ltd.',
+        name: 'Cube SoftTech Co.,Ltd.',
         logo: 'ng-logo-cube',
-        year: '2021 - Present',
+        year: 'Jul 2021 - Apr 2022',
+        age: this.calculateAge(new Date(2021, 6, 1), new Date(2022, 3, 30)),
         url: 'http://www.cubesofttech.com/',
         icon: PrimeIcons.BOX,
-        color: "#ea124e",
+        color: '#ea124e',
         systems: [
           {
             name: 'Digital Student Loan Fund System',
-            year: '2021 - Present',
+            year: 'Jul 2021 - Apr 2022',
             position: 'FullStack Developer',
-            outsource: "Outsource @ KTB",
+            outsource: 'Outsource @ KTB',
             tools: 'Micronaut Spring Boot Vue.js SAP-Crystal Reports ',
-            url: 'https://www.studentloan.or.th/'
-          }
+            url: 'https://www.studentloan.or.th/',
+          },
+        ],
+      },
+      {
+        name: 'Prior Solution Co.,Ltd.',
+        logo: 'ng-logo-prior',
+        year: 'May 2022 - Aug 2022',
+        age: this.calculateAge(new Date(2022, 4, 1), new Date(2022, 7, 31)),
+        url: 'https://www.priorsolution.co.th/',
+        icon: PrimeIcons.DESKTOP,
+        color: '#a7c957',
+        systems: [
+          {
+            name: 'คนละครึ่ง Delivery',
+            year: 'May 2022 - Aug 2022',
+            position: 'Backend Developer',
+            outsource: 'infinitas by krungthai',
+            tools: 'Spring Boot My',
+            url: 'https://www.studentloan.or.th/',
+          },
+        ],
+      },
+      {
+        name: 'SCB – Siam Commercial Bank',
+        logo: 'ng-logo-scb',
+        year: 'Sep 2022 - Nov 2022',
+        age: this.calculateAge(new Date(2022, 8, 1), new Date(2022, 10, 30)),
+        url: 'https://www.scb.co.th/',
+        icon: PrimeIcons.BOX,
+        color: '#c77dff',
+        systems: [
+          {
+            name: 'Sales and Service Solution',
+            year: 'Sep 2022 - Nov 2022',
+            position: 'Professional',
+            // outsource: 'Outsource @ KTB',
+            tools: 'Spring Boot Next.js Mui MongoDB Jenkins Kubernetes Docker',
+            // url: 'https://www.studentloan.or.th/',
+          },
+        ],
+      },
+      {
+        name: 'CardX Thailand',
+        logo: 'ng-logo-cardx',
+        year: 'Dec 2022 - Present',
+        age: this.calculateAge(new Date(2022, 11, 1), new Date()),
+        url: 'https://www.cardx.co.th/',
+        icon: PrimeIcons.BOX,
+        color: '#72ddf7',
+        systems: [
+          {
+            name: 'Sales and Service Solution',
+            year: 'Dec 2022 - Jul 2024',
+            position: 'Professional',
+            // outsource: 'Outsource @ KTB',
+            tools: 'Spring-Boot Next.js Mui MongoDB Redis Jenkins Kubernetes Docker',
+            // url: 'https://www.studentloan.or.th/',
+          },
+          {
+            name: 'API Platform Solution',
+            year: 'Aug 2024 - Present',
+            position: 'Professional',
+            // outsource: 'Outsource @ KTB',
+            tools: 'Spring-Boot Next.js Mui MongoDB Jasper-Report Kafka Redis Jenkins Kubernetes Docker',
+            // url: 'https://www.studentloan.or.th/',
+          },
         ],
       },
     ];
